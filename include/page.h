cfa0b792c775b2b502f389decdbc8af5e6eb551e
Yusuf <yusufteppei11@gmail.com>
2026-04-11 17:20:59 +0100

    helper functions in writing tuple to page

--- include/page.h
+++ include/page.h
@@ -6,13 +6,9 @@
 #include "connection.h"
 #include "data.h"
 
-
-
 #define PAGE_SIZE 128 // 4096  // KEPT TINY FOR DEVELOPMENT
 #define BUFFERPOOL_SIZE 1024*1024*1024
-#define MAX_LIVE_PAGE_COUNT BUFFERPOOL_SIZE / PAGE_SIZE  - 10  //10 is a safe estm8 4now
-
-
+#define PAGE_COUNT BUFFERPOOL_SIZE / PAGE_SIZE  - 10  //10 is a safe estm8 4now
 #define PAGE_STATUS_FREE    (1 << 0)
 #define PAGE_STATUS_OCCUPIED (1 << 1)
 #define PAGE_STATUS_LOADING (1 << 2)
@@ -21,6 +17,8 @@
 #define FLAG_DIRTY   (1 << 1)
 #define FLAG_IO_BUSY (1 << 2)
 #define FLAG_LOCKED (1 << 3)
+
+#define MAKE_VIRTUAL_ID(t, p) (((uint64_t)(t) << 32) | (p))
 
 
 typedef struct {
@@ -31,40 +29,27 @@ extern GlobalControl *global_control;
 
 
 typedef struct {
-  int64_t table_oid;
-  TableMetadata *metadata;
-  int32_t page_id;
-}
-PageTableEntry;
-
-typedef struct {
-  PageTableEntry *page_table_entries;
-  int32_t entry_count;
-}
-PageTable;
-extern PageTable *page_table;
-
-typedef struct {
-  int64_t page_id;
-  int64_t table_oid;
-  int8_t status;
-  int8_t flags;
-  int32_t empty_start_offset;
-  int32_t empty_end_offset; // 4 BYTES(DIGITS) SINCE PAGES ARE 4096 BYTES(4 DIGITS)
+  uint32_t table_oid;
+  uint32_t page_id;
+  uint32_t *next_page;
+  uint32_t empty_start_offset;
+  uint32_t empty_end_offset; 
+  uint8_t status;
+  uint8_t flags;
 }
 PageHeader;
 
 
 typedef struct {
-  int64_t page_id;
-  int64_t offset;
+  uint32_t tuple_oid;
+  uint32_t offset;
 }
 Slot;
 
 
 typedef struct {
-    int64_t size;
-    char data[];
+    uint32_t full_entry_size;
+    Tuple tuple;
 }
 PageEntry;
 
@@ -75,6 +60,23 @@ typedef struct {
   PageEntry *page_entries;
 }
 Page;
+
+typedef struct {
+  uint32_t table_oid;
+  uint64_t virtual_page_id; // [table_oid][page_id]
+  TableMetadata *metadata;
+  //Page *page;
+  uint32_t page_index;
+}
+PageTableEntry;
+
+typedef struct {
+  PageTableEntry *page_table_entries;
+  uint32_t entry_count;
+}
+PageTable;
+extern PageTable *page_table;
+
 
 typedef struct {
   GlobalControl global_control;
@@ -89,12 +91,24 @@ int64_t use_next_oid();
 
 Page *get_start_page_from_table_oid ( int64_t table_oid );
 
-void create_page_table_entry ( PageTableEntry page_table_entry);
+void create_page_table_entry ( uint32_t, uint32_t);
 
 Page *load_table( Context *context, int table_oid );
 
+Page *send_page_to_bufferpool ( Page page );
+
 Tuple *read_tuple ( int page_id, int32_t offset );
 
-Tuple *write_tuple ();
+void write_tuple_into_page( Page *page, Tuple tuple );
+
+int32_t *write_tuple (int64_t table_oid, Tuple tuple);
+
+Page *get_page_address(uint32_t table_oid, uint32_t page_id);
+
+void create_page_table_entry( uint32_t table_oid, uint32_t page_id );
+
+void load_page( uint32_t table_oid, uint32_t page_id );
+
+Page *load_next_write_page( uint32_t table_oid, uint32_t tuple_size);
 #endif
 
--- src/page.c
+++ src/page.c
@@ -15,11 +15,14 @@
 #include "page.h"
 
 ////////////////////////   GLOBAL_CONTROL    //////////////////////////
+//
 int64_t peek_next_oid() {
   
   return atomic_load(&bufferpool->global_control.next_oid);
 };
 
+//////////////////////////
+//
 int64_t use_next_oid() {
   return atomic_fetch_add(&bufferpool->global_control.next_oid, 1);
 }
@@ -28,49 +31,7 @@ int64_t use_next_oid() {
 
 
 /////////////////////     PAGES HANDLING     ///////////////////////////
-
-/*
-PageTable *load_page_table( ) {
-  FILE *file = fopen(PAGE_TABLE_FILE, "rb");
-  PageTable *page_table = NULL;
-  fseek(file, 0, SEEK_END);
-  long int file_size = ftell(file);
-  fseek(file, 0, SEEK_SET);
-  fread(page_table, file_size, 1, file);
-  return page_table;
-};
-*/
-
-
-void register_table (  ) {
-
-};
-
-
-Page *get_start_page_from_table_oid ( int64_t table_oid ) {
-  // CHECK PAGE TABLE ( FROM MEMORY ) TO FIND PAGE ADDRESS
-  PageTableEntry *curr_table_entry;
-  for (long int i=page_table->page_table_entries; i<sizeof(table_oid);i+=sizeof(PageTableEntry)){
-    curr_table_entry = i;    
-    if ( curr_table_entry->table_oid == table_oid ) {
-        
-        return bufferpool->pages + (PAGE_SIZE)*(curr_table_entry->page_id);
-    }
-  };
-  return NULL;
-};
-
-void create_page_table_entry ( PageTableEntry page_table_entry){
-
-  PageTableEntry *pte;
-  pte = page_table->page_table_entries + sizeof(PageEntry) * page_table->entry_count ;
-  page_table->entry_count++;
-
-  printf("Page Table Entry Created.\n %64d : %32d \nEntry Count : %32d",page_table->page_table_entries[page_table->entry_count-1].table_oid, page_table->page_table_entries[page_table->entry_count-1].page_id, page_table->entry_count );
-  
-};
-
-
+//
 Page *load_table( Context *context, int table_oid ){
   // PULL TABLE FROM STORAGE INTO PAGES, UPDATE PAGE TABLE; RETURN START PAGE ADDRESS
   
@@ -92,19 +53,151 @@ Page *load_table( Context *context, int table_oid ){
 
   return 0;
 }
-
-
-Tuple *read_tuple ( int page_id, int32_t offset ) {
+///////////////////////////////////////
+// SEND PAGE TO BUFFERPOOL USING HASH - IGNORE COLLISION FOR NOW
+Page *send_page_to_bufferpool(Page page){
+  uint64_t virtual_id = MAKE_VIRTUAL_ID(page.page_header.table_oid, page.page_header.page_id);
+  uint32_t index = get_hash(virtual_id, PAGE_COUNT);
   
+
+  bufferpool->pages[index] = page;
+  return &bufferpool->pages[index];
+
+}
+
+///////////////////////////////////////
+//
+Tuple *read_tuple ( int page_id, int32_t offset ) {
+  // CHECK PAGE TABLE 
   return 0;
 };
 
-Tuple *write_tuple () {
+//////////////////////////////////////
+//
+void write_tuple_into_page(Page *page,  Tuple tuple ){
+  uint32_t start = page->page_header.empty_start_offset;
+  uint32_t end = page->page_header.empty_end_offset;
+
+  // SET PAGE LOCK
+
+  // SET PAGE AS DIRTY
+
+  // WRITE SLOT
+  Slot s;
+  s.tuple_oid=tuple.tuple_oid;
+  s.offset=end - sizeof(tuple);
+  
+  *(page->slots + start - sizeof(PageHeader)) = s;
+
+  // UPDATE EMPTY_START_OFFSET
+  page->page_header.empty_start_offset += sizeof(s);
+
+  // WRITE PAGE ENTRY
+  PageEntry pe;
+  pe.full_entry_size = sizeof(tuple) + sizeof(pe.full_entry_size);
+  pe.tuple = tuple;
+  
+  uint32_t page_entries_size = sizeof(Page) - end;
+  page->page_entries = page + sizeof(Page) - page_entries_size;
+  
+  *(page->page_entries - sizeof(pe)) = pe;
+
+  // UPDATE EMPTY_END_OFFSET
+  page->page_header.empty_end_offset - sizeof(tuple);
+
+}
+
+//////////////////////////////////////
+//
+int32_t *write_tuple (int64_t table_oid, Tuple tuple) {
+
+  // LOAD NEXT WRITE PAGE
+  Page *page = load_next_write_page(table_oid, sizeof(tuple));
+  
+  // WRITE TUPLE INTO PAGE
+  page + page->page_header.empty_start_offset;
+
+  // MARK PAGE AS DIRTY
+
 
   return 0;
 }
 
-
 ///////////////////////////////////////////////////////////////////
 
+///////////////////////////  PAGE TABLE    ///////////////////////
+Page *get_page_address(uint32_t table_oid, uint32_t page_id){
+  uint64_t virtual_page_id = (uint64_t)table_oid << 32 | page_id;
 
+}
+
+/////////////////////////////////////
+//
+void create_page_table_entry ( uint32_t table_oid, uint32_t page_id){
+  PageTableEntry pte;
+  int32_t entry_count = page_table->entry_count;
+  page_table->page_table_entries[entry_count] = pte ;
+  page_table->entry_count++;
+
+  printf("Page Table Entry Created.\n %64d : %64d \nEntry Count : %32d",page_table->page_table_entries[page_table->entry_count-1].table_oid, page_table->page_table_entries[page_table->entry_count-1].virtual_page_id, page_table->entry_count );
+  
+};
+
+////////////////////////////////////
+//
+void load_page( uint32_t table_oid, uint32_t page_id ) {
+  // GET PAGE FROM STORAGE
+   char filename = DATABASES_BASE_DIR;
+   strcat(filename, int_to_string(ctx->database_oid));
+  
+   FILE *f = fopen(filename, "rb");
+   fseek(f, 0, SEEK_END);
+   int64_t filesize = ftell(f);
+    
+   Page page;
+   if ( fseek(f, page_id*PAGE_SIZE, SEEK_SET) != 0) {
+      perror("Page seek error: \n");
+   } else {
+    fread(&page, sizeof(Page), 1, f);
+    }
+
+  // CREATE PAGE TABLE ENTRY
+  TableMetadata *metadata = get_table_metadata(table_oid);
+  if ( metadata == NULL ){
+    perror("Table Metadata not found in memory"); 
+  } else {
+    create_page_table_entry(table_oid, page_id);
+  }
+
+  // PUSH PAGE TO BUFFERPOOL
+  send_page_to_bufferpool(page);
+
+}
+
+/////////////////////////////////////
+//  
+Page *load_next_write_page( uint32_t table_oid, uint32_t tuple_size) {
+
+  // CHECK THE LAST "PAGE" FOR THE FILE AND CONFIRM SPACE REMAINING
+   FILE *f = get_table_file(table_oid, "rb");
+   fseek(f, 0, SEEK_END);
+   int64_t filesize = ftell(f);
+   Page page;
+   fseek(f, -sizeof(Page), SEEK_END);
+   
+   fread(&page, sizeof(Page), 1, f);
+   uint32_t page_count = filesize / sizeof(Page);
+
+   // IF NOT ENOUGH, CREATE NEW PAGE
+   if ( (page.page_header.empty_end_offset - page.page_header.empty_start_offset) < sizeof(Page) ) {
+     Page new_page;
+     uint32_t next_page_id = page_count + 1;  
+     new_page.page_header.page_id = next_page_id;
+     new_page.page_header.table_oid = table_oid;
+     return &new_page;
+   }
+   
+   return &page;
+}
+
+/////////////////////////////////////////////////////////////////
